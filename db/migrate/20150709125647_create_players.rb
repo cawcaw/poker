3d20eed380c5b1060d6564fbd0d7db814002ba25
Dmitry Grach <dmitrygrach@gmail.com>
2015-07-09 16:06:56 +0300

    class Player

---
+++ db/migrate/20150709125647_create_players.rb
@@ -0,0 +1,13 @@
+class CreatePlayers < ActiveRecord::Migration
+  def change
+    create_table :players do |t|
+      t.string :name
+      t.integer :bankroll
+      t.boolean :horse, default: true
+
+      t.string :token
+
+      t.timestamps null: false
+    end
+  end
+end
